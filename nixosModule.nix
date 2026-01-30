f186f0e126cdbdebc0627bedccb76eb105fdb751
xvrqt <git@xvrqt.com>
2026-01-30 12:37:02 -0800

    Fixed how rust packages are optimized.

--- nixosModule.nix
+++ nixosModule.nix
@@ -1,4 +1,59 @@
-{ lib, pkgs, utils, config, ... }:
+{ lib, pkgs, config, ... }:
+let
+  # Overrides a derivation to include new CFLAGS alongside existing CFLAGS
+  # flags is a list of a strings representing the CFLAGS to be set
+  optimizeWithFlags = pkg: flags:
+    pkgs.lib.overrideDerivation pkg (old:
+      let
+        newflags = pkgs.lib.foldl' (acc: x: "${acc} ${x}") "" flags;
+        oldflags =
+          if (pkgs.lib.hasAttr "NIX_CFLAGS_COMPILE" old)
+          then "${old.NIX_CFLAGS_COMPILE}"
+          else "";
+      in
+      {
+        NIX_CFLAGS_COMPILE = "${oldflags} ${newflags}";
+      });
+
+  # Ensure all possible optimizations for this machine
+  optimizeForThisMachine = pkg:
+    optimizeWithFlags pkg [
+      # Optimize level 3 (highest level)
+      "-O3"
+      # Optimize using specific microcode and ISA implementations for this machine
+      "-march=native"
+      # No relative pointer index offets (Position Independent Code)
+      "-fPIC"
+    ];
+
+  # Harden the code to make it less likely to be expoloited
+  compileTimeHardening = pkg:
+    optimizeWithFlags pkg [
+      # Warn when printf/scanf don't use string literals
+      "-Wformat"
+      "-Wformat-security"
+      "-Werror=format-security"
+      # Add overwrite canaries to buffers greater than 4 bytes
+      "-fstack-protector-strong"
+      "--param ssp-buffer-size=4"
+      # Add buffer overflow checks at compile and at runtime; no using %n in
+      # string formatting functions (e.g. printf()) unless they are read only
+      "-O2"
+      "-D_FORTIFY_SOURCE=2"
+      # No relative pointer index offets (Position Independent Code) and makes
+      # ASLR possible
+      "-fPIC"
+      # Adds interger overflow checking to prevent errors
+      "-fno-strict-overflow"
+    ];
+
+  # Optimize for Rust
+  optimizeRustPkg = pkg:
+    pkgs.lib.overrideDerivation pkg (old: {
+      RUSTFLAGS =
+        (old.RUSTFLAGS or "") + " -C target-cpu=native";
+    });
+in
 {
   options = {
     defaults = {
@@ -29,8 +84,10 @@
 
   config =
     let
-      harden = pkg: (if config.defaults.packages.optimize then (utils.compileTimeHardening pkg) else pkg);
-      optimize = pkg: (if config.defaults.packages.optimize then (utils.optimizeForThisMachine pkg) else pkg);
+      harden = pkg: (if config.defaults.packages.optimize then (compileTimeHardening pkg) else pkg);
+      # I don't have any gcc programs, but you might
+      optimizeC = pkg: (if config.defaults.packages.optimize then (optimizeForThisMachine pkg) else pkg);
+      optimizeRust = pkg: (if config.defaults.packages.optimize then (optimizeRustPkg pkg) else pkg);
       editor = config.defaults.editor;
       auditCheck = config.defaults.auditing;
     in
@@ -58,7 +115,7 @@
         };
       };
       # Only keep 10 generations maximum
-      boot.loader.systemd-boot.configurationLimit = 10;
+      boot.loader.systemd-boot.configurationLimit = lib.mkDefault 10;
 
       # Timezone to the West Coast (Best Coast) by default
       time.timeZone = lib.mkDefault "America/Los_Angeles";
@@ -113,9 +170,9 @@
         # These packages are automatically available to all users
         systemPackages = [
           # Default text editor
-          (optimize pkgs."${editor}")
+          (optimizeRust pkgs."${editor}")
           # Pretty print system information upon shell login
-          (optimize pkgs.hyfetch)
+          (optimizeRust pkgs.hyfetch)
         ];
         # Remove all other default packages so nothing sneaks in
         defaultPackages = lib.mkDefault [ ];
