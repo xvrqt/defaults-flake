d5f98455b3e93aa6abd6af4fa69769640a59e073
xvrqt <git@xvrqt.com>
2026-01-30 13:43:56 -0800

    Removed the nixos-cli input. Added in openssh defaults.

--- flake.nix
+++ flake.nix
@@ -3,10 +3,6 @@
     # Used to keep the other inputs in lock-step
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
-    # Nicer rebuild utility
-    nix-cli.url = "github:nix-community/nixos-cli";
-    nix-cli.inputs.nixpkgs.follows = "nixpkgs";
-
     # Used to track options
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
@@ -16,7 +12,7 @@
   };
 
   outputs =
-    { nix-cli, nixpkgs, wrapper, nix-index-database, ... }:
+    { nixpkgs, wrapper, nix-index-database, ... }:
     wrapper.lib.eachDefaultSystem
       (system: {
         nixosModules =
@@ -27,7 +23,6 @@
               in
               {
                 imports = [
-                  nix-cli.nixosModules.nixos-cli
                   nix-index-database.nixosModules.nix-index
                   (import ./nixosModule.nix { inherit lib pkgs config; })
                 ];
