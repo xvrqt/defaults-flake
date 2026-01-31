fa6b60b5a71cf5246f8b49e9c5459664e65817a7
xvrqt <git@xvrqt.com>
2026-01-30 17:39:22 -0800

    Removed the flake-utils input:

--- flake.nix
+++ flake.nix
@@ -6,27 +6,30 @@
     # Used to track options
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Used to generate per-system modules
-    wrapper.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { nixpkgs, wrapper, nix-index-database, ... }:
-    wrapper.lib.eachDefaultSystem
-      (system: {
-        nixosModules =
-          {
-            default = { lib, config, ... }:
-              let
-                pkgs = nixpkgs.legacyPackages.${system};
-              in
-              {
-                imports = [
-                  nix-index-database.nixosModules.nix-index
-                  (import ./nixosModule.nix { inherit lib pkgs config; })
-                ];
-              };
+    { nixpkgs, nix-index-database, ... }:
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+        ]
+          (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+
+      nixosModules = forAllSystems
+        (pkgs: {
+          default = { lib, config, ... }: {
+            imports = [
+              nix-index-database.nixosModules.nix-index
+              (import ./nixosModule.nix {
+                inherit lib pkgs config;
+              })
+            ];
           };
-      });
+        });
+    };
 }
